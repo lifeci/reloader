3fd01b58874d1cf652266a6b6c2c6a8d7f65171e
Roman Judin <RJ@vistex.com>
2019-07-23 14:14:52 +0200

    fixing bugs with quotes: ', "

--- loop.sh
+++ loop.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-files_pattern=$FILES_PATTERN;
-action=$ACTION;
+files_pattern="$FILES_PATTERN";
+action="$ACTION";
 check_interval=$CHECK_INTERVAL
 
-if [ -z $files_pattern ] || [ -z $action ]; then
+if [ -z "$files_pattern" ] || [ -z "$action" ]; then
   echo "ERROR: [ files_pattern: $files_pattern || action: $action ]";
   exit 1
 fi;
@@ -18,7 +18,7 @@ get_sha(){
   cat $files_pattern | sha256sum | cut -f 1 -d " "
 }
 
-sum_current=`get_sha $files_pattern`;
+sum_current=`get_sha "$files_pattern"`;
 
 echo "I'll monitor every check_interval=$check_interval files_pattern=$files_pattern";
 echo "in case if it will be changed => apply action: $action";
@@ -26,7 +26,7 @@ echo "in case if it will be changed => apply action: $action";
 while true
 do
 
-  sum_check=`get_sha $files_pattern`;
+  sum_check=`get_sha "$files_pattern"`;
 
   if [ "$sum_current" != "$sum_check" ]; then
     echo "`date` :: executing: $action";
